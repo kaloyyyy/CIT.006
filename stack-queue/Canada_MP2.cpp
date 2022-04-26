d86d69a60cc247c0e1fa42800dcf2d2b0b6700dc
kaloyyyy <70120607+kaloyyyy@users.noreply.github.com>
2022-04-27 02:48:01 +0800

    Canada_MP2.cpp update

--- stack-queue/Canada_MP2.cpp
+++ stack-queue/Canada_MP2.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 using namespace std;
 
-struct node
-{
+struct node {
     int share;
     int price;
     node *next;
@@ -13,54 +12,42 @@ node *rear = nullptr;
 int gainLoss, totalShare;
 
 //check if front and rear is NULL
-bool isEmpty()
-{
-    if (front == nullptr && rear == nullptr)
-    {
+bool isEmpty() {
+    if (front == nullptr && rear == nullptr) {
         return true;
-    }
-    else
-    {
+    } else {
         return false;
     }
 }
+
 //traverse through the nodes and prints the share and price at the time bought.
-void printShare()
-{
-    if (isEmpty())
-    {
+void printShare() {
+    if (isEmpty()) {
         cout << "portfolio is empty\n";
-    }
-    else
-    {
+    } else {
         node *temp = front;
-        cout << endl << "total shares: " << totalShare<<endl;
-        cout << "shares --- price"<<endl;
-        while (temp != nullptr)
-        {
-            cout << temp->share << " --- " << temp->price<<" php" << endl;
+        cout << endl << "total shares: " << totalShare << endl;
+        cout << "shares --- price" << endl;
+        while (temp != nullptr) {
+            cout << temp->share << " --- " << temp->price << " php" << endl;
             temp = temp->next;
         }
         cout << endl;
     }
 }
+
 //adds a node/buy to the shares
-void buyShareFunc(int share, int price)
-{
+void buyShareFunc(int share, int price) {
     node *temp = front;
     node *newNode = new node;
     newNode->share = share;
     newNode->price = price;
     newNode->next = nullptr;
-    if (isEmpty())
-    {
+    if (isEmpty()) {
         //sets the new and first node as the head/front
         front = newNode;
-    }
-    else
-    {
-        while (temp->next != nullptr)
-        {
+    } else {
+        while (temp->next != nullptr) {
             //traverse the linked list
             temp = temp->next;
         }
@@ -68,61 +55,48 @@ void buyShareFunc(int share, int price)
         temp->next = newNode;
     }
     cout << "shares bought!\n";
-    totalShare = totalShare+ newNode->share;
+    totalShare = totalShare + newNode->share;
     printShare();
 }
+
 //may or may not sellShare depends on the shares to be sold.
 //sell function based on sellShare
-void sellShareFunc(int sell, int price)
-{
+void sellShareFunc(int sell, int price) {
     int prevPrice;
     int shareSell = sell;
-    if (isEmpty())
-    {
+    if (isEmpty()) {
         cout << "queue is empty\n";
-    }
-    else if(sell >totalShare)
-    {
+    } else if (sell > totalShare) {
         cout << "You don't have enough shares to sell\n";
         printShare();
-    }
-    else
-    {
+    } else {
         totalShare = totalShare - shareSell;
         // Move the head pointer to the next node
-        do
-        {
-            if (shareSell >= front->share)
-            {
+        do {
+            if (shareSell >= front->share) {
                 node *temp = front;
                 prevPrice = temp->price;
                 gainLoss = gainLoss + (front->share * (price - prevPrice));
                 shareSell = shareSell - front->share;
                 front = front->next;
                 delete temp;
-            }
-            else if (shareSell < front->share)
-            {
-                gainLoss = gainLoss + (shareSell* (price - front->price));
+            } else if (shareSell < front->share) {
+                gainLoss = gainLoss + (shareSell * (price - front->price));
                 front->share = front->share - shareSell;
                 shareSell = 0;
             }
-        }
-        while (shareSell != 0);
+        } while (shareSell != 0);
     }
 }
 
 
-int main()
-{
+int main() {
     int buyShares, sharePrice, sellShares;
     char choice = 'x';
-    do
-    {
+    do {
         cout << "a.buy  b.sell   c.check   d.exit\n";
         cin >> choice;
-        switch (choice)
-        {
+        switch (choice) {
             case 'a'://buyShare
                 cout << "enter number of shares:\n";
                 cin >> buyShares;
@@ -137,7 +111,7 @@ int main()
                 cin >> sharePrice;
                 sellShareFunc(sellShares, sharePrice);
                 printShare();
-                cout<<"profit: "<<gainLoss<<endl;
+                cout << "profit: " << gainLoss << endl;
                 break;
             case 'c':
                 printShare();
@@ -146,7 +120,6 @@ int main()
                 cout << "enter a valid choice\n";
                 break;
         }
-    }
-    while (choice != 'd');
+    } while (choice != 'd');
     return 0;
 }
