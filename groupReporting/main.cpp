99900ddc08cf5ed1b0123d67d669ac86ef7e9477
kaloyyyy <70120607+kaloyyyy@users.noreply.github.com>
2022-04-27 02:48:55 +0800

    add groupReporting

---
+++ groupReporting/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "treeTraverse.h"
+#include "avlTree.h"
+#include "sort.h"
+#include <cstring>
+using namespace std;
+
+
+int main() {
+    cout << "what is your topic?" << endl;
+    char choice = 'n';
+    char order = 'n';
+    do {
+        cout << "\n\na. tree traversal    b. AVL Tree    c. sorting algorithms    x. exit\n";
+        cin >> choice;
+        switch (choice) {
+            case 'a':
+                treeTraversal();
+                break;
+            case 'b':
+                avlMenu();
+                break;
+            case 'c':
+                sort();
+                break;
+            default:
+                cout << "invalid choice\n";
+                break;
+        }
+    } while (choice != 'x');
+
+    return 0;
+}
+
+
+
