90a058e0bc33916a281b416dad1848b9931fd85e
kaloyyyy <70120607+kaloyyyy@users.noreply.github.com>
2022-09-25 20:17:44 +0800

    extra cred 0

--- Automata/main.cpp
+++ Automata/main.cpp
@@ -4,24 +4,26 @@
 using namespace std;
 int main(){
     string myText, set1, set2;
-    int cases, elType, opers, opType, set, item;
+    int cases, elType, opers, opType, set, item, caseCount;
     ifstream myReader("mpa1.in");
     myReader>>cases;
     cout<<"number of cases: "<<cases<<endl;
-    myReader>>elType;
-    cout<<"element type: "<<elType<<endl;
-    myReader.ignore();
-    getline(myReader, set1);
-    cout<<"set 1: "<<set1<<endl;
-    getline(myReader,set2);
-    cout<<"set 2: "<<set2<<endl;
-    myReader>>opType;
-    cout<<"operation: "<<opType<<endl;
-
-
-    while(getline (myReader, myText)){
-        cout<<myText<<endl;
+    caseCount = 1;
+    while(cases>0){
+        cout<<"\n-------start of case "<<caseCount<<"------\n";
+        myReader>>elType;
+        cout<<"element type: "<<elType<<endl;
+        myReader.ignore();
+        getline(myReader, set1);
+        cout<<"set 1: "<<set1<<endl;
+        getline(myReader,set2);
+        cout<<"set 2: "<<set2<<endl;
+        myReader>>opType;
+        cout<<"operation: "<<opType<<endl;
+        cases = cases - 1;
+        cout<<"--------end of case "<<caseCount<<"-------\n";
+        caseCount++;
     }
-
+    system("pause");
     return 0;
 }
