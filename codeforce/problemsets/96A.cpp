d9a494618880af213301ee4315ca77f46d4fd231
kaloyyyy <70120607+kaloyyyy@users.noreply.github.com>
2023-03-16 10:17:03 +0800

    Create 96A.cpp

---
+++ codeforce/problemsets/96A.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    string s;
+    cin>>s;
+    char curr;
+    int count = 0;
+    for(int i = 0; i < s.size(); i++){
+        cout<<count<<endl;
+        if(curr == s[i]){
+            count++;
+            if(count == 7){
+            cout<<"YES";
+            break;
+        }
+        }else{
+            curr = s[i];
+            count = 1;
+        }
+    }
+    if(count != 7){
+        cout<<"NO";
+    }
+}
